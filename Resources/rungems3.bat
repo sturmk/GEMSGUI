dab6facb15c9962c698fd2e3aa1237555f8cc7ea
svetad <sv.dmytr@gmail.com>
2025-06-24 12:13:26 +0300

    Added readme

--- Resources/rungems3.bat
+++ Resources/rungems3.bat
@@ -1,16 +1,16 @@
-rem  Change the path to the actual location of GEMS3 executable and Resources
-cd .\Gems3-app\bin
+rem  Change the path to the actual location of gem-selektor executable and Resources
+cd ./Gems3-app/bin
 
 rem 1. First launch with default location of modeling projects (usually done by the installer)
 rem gem-selektor.exe -d > gems3.log
 
 rem 2. Normal runs in default locations (retains all settings from previous session)
-gem-selektor.exe -s . -u ..\Library\Gems3 > gems3.log
+gem-selektor.exe > gems3.log
 rem or 
-rem gem-selektor.exe > gems3.log
+rem gem-selektor.exe -s . -u C:\Users\<USER>\Library\Gems3 > gems3.log
 
 rem 2a. The above is equivalent to
-rem gem-selektor.exe -s C:\GEMS3MOD\Gems3-app\bin -u ..\Library\Gems3 > gems3.log
+rem gem-selektor.exe -s .\Gems3-app\bin -u C:\Users\<USER>\Library\Gems3 > gems3.log
 
 rem 3. New file configuration if project subfolder(s) were added/removed to/from
 rem   /projects or if /projects are not in the default location
@@ -18,10 +18,10 @@ rem gem-selektor.exe -c -s . -u G:\My_GEMS_Projects_Location\Gems3 > gems3.log
 
 rem 4. New \data\*.ini files - remake DOD and module dialog configurators
 rem Otherwise, does the same as  gems3 -c
-rem gem-selektor.exe -d -s . -u ..\Library\Gems3 > gems3.log
+rem gem-selektor.exe -d -s . -u C:\Users\<USER>\Library\Gems3 > gems3.log
 
 rem 5. Add -f after gem-selektor.exe for a write access to database files in \DB.default\
 rem (for developers only!)
 
 rem 6. Create on desktop a shortcut to ~\Gems3-app\bin\gem-selektor.exe -c
-rem   in shortcut, select the icon ~\Gems3-app\bin\Resources\Gems3.ico
+rem   in shortcut, select the icon ~\Gems3-app\bin\gem-selektor.ico
